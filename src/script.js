0e412be67531d535a2eac030d8fba0cb369e8897
pavlovator <pavlove.filip@gmail.com>
2025-03-02 20:21:26 +0100

    mnap

--- src/script.js
+++ src/script.js
@@ -1,23 +1,34 @@
-var fs = require('fs')
-  , d3 = require('d3')
-  , _ = require('underscore')
-  , europe = require('src/countries.js').countries
+// Map in the geoJsonEurope object
+let geoJsonEurope;
+let jsonEu2024;
 
-function prettyJSON(obj) {
-  console.log(JSON.stringify(obj, null, 2))
-}
+d3.json("../data/europe.geojson")
+  .then(data => {
+    geoJsonEurope = data;
+    console.log("GeoJSON Loaded:", geoJsonEurope);
 
-fs.readFile('data/map.geojson', 'utf8', function(err, geojson) {
-  var map = JSON.parse(geojson)
+    // Create Svg to draw Map on
+    var width = 500, height = 500;
+    let projection = d3.geoMercator();
+    projection.fitSize([width, height], geoJsonEurope);
+    let geoGenerator = d3.geoPath().projection(projection);
 
-  fmap = map.features.filter(function(country) {
-    return _.contains(europe, country.properties.ISO2)
-  })
+    let svg = d3.select("body").append("svg")
+                .attr("width", width)
+                .attr("height", height);
+    
+    svg.selectAll('path')  // Select all path elements inside the svg
+      .data(geoJsonEurope.features)      // Bind your geojson features to the paths
+      .join('path')                // Join the new data to new <path> elements
+      .attr('d', geoGenerator)     // Set the path data using the geoGenerator
+      .attr('fill', 'lightblue')   // Optional: Set a fill color for the paths
+      .attr('stroke', 'black')     // Optional: Set a stroke color for the paths
+      .attr('stroke-width', 1);    // Optional: Set a stroke width for the paths
 
-  main = { type: 'FeatureCollection', 'features': [] }
-  main.features = fmap
+  }).catch(error => console.error("Error loading ../data/europe.geojson:", error));
 
-  main = JSON.stringify(main, null, 2)
-
-  fs.writeFile('fix.json', main)
-})
+d3.json("../data/2024/eu.json")
+  .then(data => {
+    jsonEu2024 = data;
+    console.log("Eu json Loaded:", jsonEu2024);
+  }).catch(error => console.error("Error loading ../data/2024/eu.json:", error));
